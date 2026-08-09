af6e171102c7da75751b20b754c1be5c7329a0e3
mariem-ayari163 <ayari.mariem.1@esprit.tn>
2026-08-09 10:06:04 +0100

    Add CI/CD project

---
+++ app/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/api/health", (req, res) => {
+    res.json({
+        status: "OK",
+        message: "Application opérationnelle"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
